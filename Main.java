c2378aefff2c5e9932429384181c6a39ed3e4442
takkun <takujin3811@gmail.com>
2023-02-13 13:37:19 +0900

    Section3-2
    トランプゲームの選択機能の実装

--- Main.java
+++ Main.java
@@ -1,7 +1,31 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        // 「ブラックジャック」ゲームの開始
-        Blackjack blackjack = new Blackjack();
-        blackjack.play();
+        Scanner scan = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("遊ぶゲームを選択して下さい。");
+            System.out.println("0: 戦争、1: ブラックジャック");
+            String answer = scan.nextLine();
+
+            if (answer.equals("0")) {
+                System.out.println("");
+                System.out.println("「戦争」ゲームスタート!");
+                // 戦争ゲームの開始
+                War war = new War();
+                war.play();
+                break;
+            } else if (answer.equals("1")) {
+                System.out.println("");
+                System.out.println("「ブラックジャック」ゲームスタート!");
+                // 「ブラックジャック」ゲームの開始
+                Blackjack blackjack = new Blackjack();
+                blackjack.play();
+                break;
+            }
+        }
+
+        scan.close();
     }
 }
